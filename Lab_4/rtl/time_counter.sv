3fc8b35bdfa8398b309972372788dcaf5fc3d3a4
Michael Aguero <mikeaguero19@gmail.com>
2024-07-25 18:46:20 -0700

    commiting

---
+++ Lab_4/rtl/time_counter.sv
@@ -0,0 +1,33 @@
+// Copyright (c) 2024 Ethan Sifferman.
+// All rights reserved. Distribution Prohibited.
+
+module time_counter (
+    input  logic       clk_4_i,
+    input  logic       rst_ni,
+    input  logic       en_i,
+    output logic [4:0] count_o
+);
+
+
+logic [4:0] t_count_d, t_count_q;
+
+always_ff @(posedge clk_4_i)
+    begin
+        if (!rst_ni) begin
+            t_count_q <= 0;
+        end else begin
+            t_count_q <= t_count_d;
+        end
+    end
+
+always_comb
+    begin
+        t_count_d = t_count_q;
+        if (en_i) begin
+            t_count_d = t_count_q + 1;
+        end
+    end
+
+assign count_o = t_count_q;
+
+endmodule
