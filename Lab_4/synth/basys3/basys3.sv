3047388c1de402978d4aa6dd4fb84e3d9d1f397e
mbaguero <mbaguero@ucsc.edu>
2024-07-29 13:21:51 -0700

    intial basys3 and basys master

--- Lab_4/synth/basys3/basys3.sv
+++ Lab_4/synth/basys3/basys3.sv
@@ -2,9 +2,31 @@
 // All rights reserved. Distribution Prohibited.
 
 module basys3 (
-    // TODO
+    input logic btnU,
+    input logic btnL,
+    input logic btnR,
+    input logic btnC,
+    input logic btn_en_i,
+
+    output logic [15:0] led_o,
+    output logic [3:0] anode_o,
+    output logic [6:0] segments_o
 );
 
-// TODO
+logic [3:0] operation_i;
+
+always_comb begin
+    if (btnU & btn_en_i) begin
+        operation_i = 4'b0001; // 
+    end else if (btnL & btn_en_i) begin
+        operation_i = 4'b0010;
+    end else if (btnC & btn_en_i) begin
+        operation_i = 4'b0100;
+    end else if (btnR) begin
+        operation_i = 4'b1000;
+    end else begin
+        operation_i = 4'b0000;
+    end
+end
 
 endmodule
