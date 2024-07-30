a922754ef7595679efceaccda3b40f11b5ef8195
mbaguero <mbaguero@ucsc.edu>
2024-07-30 12:02:50 -0700

    end of lab

--- Lab_4/rtl/basys3/basys3_7seg_driver.sv
+++ Lab_4/rtl/basys3/basys3_7seg_driver.sv
@@ -18,8 +18,6 @@ module basys3_7seg_driver (
     output logic [6:0] segments_o
 );
 
-
-
 logic [6:0] seg_n;
 
 hex7seg hex7seg (
@@ -45,7 +43,7 @@ always_ff @(posedge clk_1k_i)
     begin
         if (!rst_ni) begin
             digit_q <= 4'b0000;
-            anode_q <= 4'b0000;
+            anode_q <= 4'b1111;
         end else begin
             digit_q <= digit_d;
             anode_q <= anode_d;
@@ -59,16 +57,16 @@ always_comb
 
         if (digit0_en_i) begin
             digit_d = digit0_i;
-            anode_d[0] = 1;
+            anode_d[0] = 0;
         end else if (digit1_en_i) begin
             digit_d = digit1_i;
-            anode_d[1] = 1;
+            anode_d[1] = 0;
         end else if (digit2_en_i) begin
             digit_d = digit2_i;
-            anode_d[2] = 1;
+            anode_d[2] = 0;
         end else if (digit3_en_i) begin
             digit_d = digit3_i;
-            anode_d[3] = 1;
+            anode_d[3] = 0;
         end
     end
 
