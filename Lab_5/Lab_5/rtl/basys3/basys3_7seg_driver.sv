ee9ef87d55c824a7d0a44ea31f40976896fedf94
Michael Aguero <mikeaguero19@gmail.com>
2024-08-06 18:04:45 -0700

    finished lfsr16, used lab4 code for hex7

--- Lab_5/Lab_5/rtl/basys3/basys3_7seg_driver.sv
+++ Lab_5/Lab_5/rtl/basys3/basys3_7seg_driver.sv
@@ -18,6 +18,95 @@ module basys3_7seg_driver (
     output logic [6:0] segments_o
 );
 
-// TODO
+
+logic [6:0] seg_n;
+
+logic [3:0] digit_i;
+
+hex7seg hex7seg_inst (
+    .d3(digit_i[3]),
+    .d2(digit_i[2]),
+    .d1(digit_i[1]),
+    .d0(digit_i[0]),
+
+    .A(seg_n[0]),
+    .B(seg_n[1]),
+    .C(seg_n[2]),
+    .D(seg_n[3]),
+    .E(seg_n[4]),
+    .F(seg_n[5]),
+    .G(seg_n[6])
+);
+
+logic [3:0] count_d, count_q;
+
+always_ff @(posedge clk_1k_i)
+    begin
+        if(!rst_ni) begin
+            count_q <= 2'b00;
+        end else begin
+            count_q <= count_d;
+        end
+    end
+
+assign count_d = count_q + 1;
+
+always_comb
+    begin
+        //seg_n = 7'b1111111;
+        anode_o = 4'b1111;
+        digit_i = 4'b0000;
+        case (count_q)
+            2'b00: begin
+                if (digit0_en_i) begin
+                    digit_i = digit0_i;
+                    anode_o = 4'b1110;
+                    //seg_n = 7'b0000000;
+                end else begin
+                    anode_o = 4'b1111;
+                end
+            end
+            2'b01: begin
+                if (digit1_en_i) begin
+                    digit_i = digit1_i;
+                    anode_o = 4'b1101;
+                    //seg_n = 7'b0000000;
+                end else begin
+                    anode_o = 4'b1111;
+                end
+            end
+            2'b10: begin
+                if (digit2_en_i) begin
+                    digit_i = digit2_i;
+                    anode_o = 4'b1011;
+                    //seg_n = 7'b0000000;
+                end else begin
+                    anode_o = 4'b1111;
+                end
+            end
+            2'b11: begin
+                if (digit3_en_i) begin
+                    digit_i = digit3_i;
+                    anode_o = 4'b0111;
+                    //seg_n = 7'b0000000;
+                end else begin
+                    anode_o = 4'b1111;
+                end
+            end
+            default: begin
+                digit_i = 4'b0000;
+                anode_o = 4'b1111;
+            end
+        endcase
+    end
+
+always_comb
+    begin
+        if (anode_o == 4'b1111) begin
+            segments_o = 7'b1111111;
+        end else begin
+            segments_o = ~seg_n;
+        end
+    end
 
 endmodule
--- Lab_5/Lab_5/rtl/lfsr16.sv
+++ Lab_5/Lab_5/rtl/lfsr16.sv
@@ -9,6 +9,21 @@ module lfsr16 (
     output logic [15:0] rand_o
 );
 
-// TODO
+logic [15:0] rnd_d, rnd_q;
+logic feedback_xor;
+
+assign feedback_xor = rnd_q[15] ^ rnd_q[14] ^ rnd_q[12] ^ rnd_q[3];
+assign rnd_d = {rnd_q[14:0], feedback_xor};
+
+always_ff @(posedge clk_i )
+    begin
+        if (!rst_ni) begin
+            rnd_q <= 16'b0000000000000001;
+        end else if (next_i) begin
+            rnd_q <= rnd_d;
+        end
+    end
+
+assign rand_o = rnd_q[15:0];
 
 endmodule
