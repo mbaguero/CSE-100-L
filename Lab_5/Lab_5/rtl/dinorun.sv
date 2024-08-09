13a33a30f841352fbd02b036533af2378fcbcabd
Michael Aguero <mikeaguero19@gmail.com>
2024-08-08 17:24:38 -0700

    initial of dino run and state machine

--- Lab_5/Lab_5/rtl/dinorun.sv
+++ Lab_5/Lab_5/rtl/dinorun.sv
@@ -26,16 +26,15 @@ module dinorun import dinorun_pkg::*; (
 );
 
 // Edge Detector
-logic edge_en;
-logic edge_dectect;
+logic next_frame;
 
 edge_detector edge_detector_inst (
     //inputs
-    .clk_i(clk_25_175_i);
-    .data_i(edge_en);
+    .clk_i(clk_25_175_i),
+    .data_i(vsync_q),
 
     //outputs
-    .edge_o(edge_dectect)
+    .edge_o(next_frame)
 );
 
 // 16 bit LFSR
@@ -45,9 +44,9 @@ logic lfsr_en;
 
 lfsr16 lfsr16_inst (
     //inputs
-    .clk_i(clk_25_175_i);
-    .rst_ni(rst_ni);
-    .next_i(lfsr_en);
+    .clk_i(clk_25_175_i),
+    .rst_ni(rst_ni),
+    .next_i(lfsr_en),
 
     //outputs
     .rand_o(rand_num)
@@ -55,57 +54,59 @@ lfsr16 lfsr16_inst (
 
 // Score Counter
 
-loigc score_counter_en;
+logic score_counter_en,
+logic score_rst_ni,
 
 score_counter score_counter_inst (
     //inputs
-    .clk_i(clk_25_175_i);
-    .rst_ni(rst_ni);
-    .en_i(score_counter_en);
+    .clk_i(clk_25_175_i),
+    .rst_ni(score_rst_ni),
+    .en_i(score_counter_en),
 
-    .digit0_o(digit0_o);
-    .digit1_o(digit1_o);
-    .digit2_o(digit2_o);
+    .digit0_o(digit0_o),
+    .digit1_o(digit1_o),
+    .digit2_o(digit2_o),
     .digit3_o(digit3_o)
 );
 
 // VGA Timer
 
-logic hsync, vsync;
+logic hsync_d, hsync_q, vsync_d, vsync_q;
 logic is_visible;
 logic [9:0] position_x, position_y;
 
 vga_timer vga_timer_inst (
     //inputs
-    .clk_i(clk_25_175_i);
-    .rst_ni(rst_ni);
+    .clk_i(clk_25_175_i),
+    .rst_ni(rst_ni),
 
     //outputs
-    .hsync_o(hsync);
-    .vsync_o(vsync);
-    .visible_o(is_visible);
-    .position_x_o(position_x);
-    .position_y_o(position_o)
+    .hsync_o(hsync_d),
+    .vsync_o(vsync_d),
+    .visible_o(is_visible),
+    .position_x_o(position_x),
+    .position_y_o(position_y)
 );
 
 // Bird Object
 
 logic bird_spawn_en; // = (rand_num[12:8] == 5'b00000 ) ? 1 : 0;
 logic bird_pixel;
+logic is_frozen;
 
 bird bird_inst (
     //inputs
-    .clk_i(clk_25_175_i);
-    .rst_ni(rst_ni);
-    .spawn_i(bird_spawn_en);
-    .next_frame_i(vsync);
+    .clk_i(clk_25_175_i),
+    .rst_ni(rst_ni),
+    .spawn_i(bird_spawn_en & !is_visible & !is_frozen),
+    .next_frame_i(next_frame & !is_frozen),
 
-    .pixel_x_i(position_x);
-    .pixel_y_i(position_y);
-    .rand_i(rand_num[1:0]);
+    .pixel_x_i(position_x),
+    .pixel_y_i(position_y),
+    .rand_i(rand_num[1:0]),
 
     //ouputs
-    .pixel_o(bird_pixel);
+    .pixel_o(bird_pixel)
 );
 
 // Cactus Object
@@ -115,17 +116,17 @@ logic cactus_pixel;
 
 cactus cactus_inst (
     //inputs
-    .clk_i(clk_25_175_i);
-    .rst_ni(rst_ni);
-    .spawn_i(cactus_spawn_en);
-    .next_frame_i(vsync);
+    .clk_i(clk_25_175_i),
+    .rst_ni(rst_ni),
+    .spawn_i(cactus_spawn_en & !is_visible & !is_frozen),
+    .next_frame_i(next_frame & !is_frozen),
 
-    .pixel_x_i(position_x);
-    .pixel_y_i(position_y);
-    .rand_i(rand_num[4:2]);
+    .pixel_x_i(position_x),
+    .pixel_y_i(position_y),
+    .rand_i(rand_num[4:2]),
 
     //ouputs
-    .pixel_o(cactus_pixel);
+    .pixel_o(cactus_pixel)
 );
 
 // Dino Object
@@ -133,17 +134,17 @@ cactus cactus_inst (
 logic is_hit;
 logic dino_pixel;
 
-dino dino_isnt (
+dino dino_inst (
     //inputs
-    .clk_i(clk_25_175_i);
-    .rst_ni(rst_ni);
-    .down_i(down_i);
-    .up_i(up_i);
-    .hit_i(is_hit);
-    .next_frame_i(vsync);
+    .clk_i(clk_25_175_i),
+    .rst_ni(rst_ni),
+    .down_i(down_i),
+    .up_i(up_i),
+    .hit_i(is_hit),
+    .next_frame_i(next_frame),
 
-    .pixel_x_i(position_x);
-    .pixel_y_i(position_y);
+    .pixel_x_i(position_x),
+    .pixel_y_i(position_y),
 
     //outputs
     .pixel_o(dino_pixel)
@@ -152,15 +153,86 @@ dino dino_isnt (
 // Title Object
 
 logic title_pixel;
+logic title_en;
 
 title title_inst (
     //inputs
-    .pixel_x_i(position_x);
-    .pixel_y_i(position_y);
+    .pixel_x_i(position_x),
+    .pixel_y_i(position_y),
 
     //ouputs
-    .pixel_o(title_pixel);
+    .pixel_o(title_pixel)
 );
+
+
+// RGB Flip Flop
+
+logic [3:0] red_d, red_q, green_d, green_q, blue_d, blue_q;
+
+always_ff @(posedge clk_25_175_i)
+    begin
+        if (!rst_ni) begin
+            vsync_q <= 0;
+            hsync_q <= 0;
+
+            red_q <= 4'b0;
+            green_q <= 4'b0;
+            blue_q <= 4'b0;
+        end else begin
+            vsync_q <= vsync_d;
+            hsync_q <= hsync_d;
+
+            red_q <= red_d;
+            green_q <= green_d;
+            blue_q <= blue_d;
+        end
+    end
+
+// Multiplexer to Select Which Pixel to Output
+    always_comb begin
+        red_d = red_q;
+        green_d = green_q; 
+        blue_d = blue_q;
+
+        if (bird_pixel) begin
+            red_d = 4'b1111; // Example color for bird (bright red)
+            green_d = 4'b0000; 
+            blue_d = 4'b0000;
+        end else if (cactus_pixel) begin
+            red_d = 4'b0000;
+            green_d = 4'b1111; // Example color for cactus (bright green)
+            blue_d = 4'b0000;
+        end else if (dino_pixel) begin
+            red_d = 4'b0000;
+            green_d = 4'b0000; 
+            blue_d = 4'b1111;
+        end else if (title_pixel & title_en) begin
+            red_d = 4'b0011;
+            green_d = 4'b0000; 
+            blue_d = 4'b1100;
+        end else begin
+            red_d = 4'b0000; // Background color
+            green_d = 4'b0000;
+            blue_d = 4'b0000;
+        end
+    end
+
+always_comb
+    begin
+        if (is_visible) begin
+            vga_red_o = red_q;
+            vga_green_o = green_q;
+            vga_blue_o = blue_q;
+        end else begin
+            vga_red_o = 4'b0;
+            vga_green_o = 4'b0;
+            vga_blue_o = 4'b0;
+        end
+    end
+
+assign vga_hsync_o = hsync_q;
+assign vga_vsync_o = vsync_q;
+
 
 // State Machine
 
@@ -175,37 +247,91 @@ always_ff @(posedge clk_i)
         end
     end
 
+
 always_comb
     begin
         state_d = state_q;
-        //TODO
+
+        //object enables
+        cactus_spawn_en = (rand_num[12:7] == 6'b101010 ) ? 1 : 0;
+        bird_spawn_en = (rand_num[12:8] == 5'b00000 ) ? 1 : 0;
+
+        // dino hit
+        is_hit = 0;
+
+        // screen freeze
+        is_frozen = 0;
+
+        // score counter en and reset
+        score_counter_en = 0;
+        score_rst_ni = 0;
+
+        // lfsr en
+        lfsr_en = 1;
+
+        // title en
+        title_en = 0;
+
+        // digit enables
+        digit0_en_o = 0;
+        digit1_en_o = 0;
+        digit2_en_o = 0;
+        digit3_en_o = 0;
+
+
 
         unique case (state_q)
             STARTING: begin
+                
+                //a) title screen should be visible 
 
-                // TODO
+                //b) dino should be running at initial --- DONE
 
+                //c) no obstacles (obstacle_en LOW) --- DONE
+
+                //d) if start_i go to state PLAYING  --- DONE
+
+                cactus_spawn_en = 0;
+                bird_spawn_en = 0;
+                score_rst_ni = 0; // begin reset to 0000
+                score_counter_en = 0;
+                title_en = 1;
+                is_frozen = 0;
+
+                if (start_i) begin
+                    state_d = PLAYING;
+                end
             end
             PLAYING: begin
+                score_rst_ni = 1;
+                score_counter_en = 1;
+                title_en = 0;
+                
+                if (dino_pixel & (bird_pixel | cactus_pixel)) begin
+                    is_hit = 1;
+                    state_d = HIT;
+                end
 
-                // TODO
-
+                
             end
             HIT: begin
+                score_counter_en = 0;
+                is_frozen = 1;
+                title_en = 0;
 
-                // TODO
-
+                if (start_i) begin
+                    cactus_spawn_en = 0;
+                    bird_spawn_en = 0;
+                    score_rst_ni = 0; // begin reset to 0000
+                    score_counter_en = 0;
+                    is_frozen = 0;
+                    state_d = PLAYING;
+                end
             end
             default: begin
                 state_d = STARTING;
             end
         endcase
-
-
-
-
-
-
 
     end
 
